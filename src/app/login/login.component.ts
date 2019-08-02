31a3a03a518786d35e52707fbf62122adaf4a7b9
timjones92 <timothy.jones6@griffithuni.edu.au>
2019-08-02 15:24:08 +1000

    Week 4 Tutorial

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,18 +10,20 @@ export class LoginComponent implements OnInit {
 
   email = "";
   password = "";
-
   error = "";
+
   cap = {"email": "cap@avengers.com", "password": "123456"}
   
   constructor(private router:Router) { }
 
   ngOnInit() {
-    
   }
 
   navURL() {
-    if ((this.email && this.password) != (this.cap.email && this.cap.password)) {
+    if (this.email == this.cap.email && this.cap.password != this.cap.password) {
+      this.error = "There is a probelm with the credentials.";
+      alert("Not a valid user!");
+    } else if (this.cap.password != this.cap.password){
       this.error = "There is a probelm with the credentials.";
       alert("Not a valid user!");
     } else {
