12fd8be15cae651977345bee30a210e1c6aeb696
timjones92 <timothy.jones6@griffithuni.edu.au>
2019-08-02 15:04:57 +1000

    Week 4 Tutorial

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  email = "";
+  password = "";
+
+  error = "";
+  cap = {"email": "cap@avengers.com", "password": "123456"}
+  
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+    
+  }
+
+  navURL() {
+    if ((this.email && this.password) != (this.cap.email && this.cap.password)) {
+      this.error = "There is a probelm with the credentials.";
+      alert("Not a valid user!");
+    } else {
+      this.router.navigateByUrl('/account');
+    }
+  };
+
+}
